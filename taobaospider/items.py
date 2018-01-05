8c8936b14a0c2271e670ac1dfdaa4309dda65aee
whbing <284734261@qq.com>
2018-01-05 20:12:43 +0800

    给item加了注释

--- taobaospider/items.py
+++ taobaospider/items.py
@@ -7,13 +7,14 @@
 
 import scrapy
 
-
+# 这个item是获取class1和class2的字段
 class TaobaoItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     class1 = scrapy.Field()
     class2 = scrapy.Field()
-    
+
+# 这个item是获取具体内容的字段    
 class TaobaoDataItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
